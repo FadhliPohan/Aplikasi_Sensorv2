4dbc9a4cbfcc3a0b2ae2cad36fcf72b05b4f6810
FadhliPohan <muhammadfadly.mfd@gmail.com>
2023-01-18 15:04:27 +0700

    10

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="assets/img/favicon.ico">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
 
@@ -15,10 +16,10 @@
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Animation library for notifications   -->
-    <link href="assets/css/animate.min.css" rel="stylesheet"/>
+    <link href="assets/css/animate.min.css" rel="stylesheet" />
 
     <!--  Light Bootstrap Table core CSS    -->
-    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet"/>
+    <link href="assets/css/light-bootstrap-dashboard.css?v=1.4.0" rel="stylesheet" />
 
 
     <!--  CSS for Demo Purpose, don't include it in your project     -->
@@ -30,78 +31,79 @@
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
     <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
 </head>
+
 <body>
 
-<div class="wrapper">
-    <div class="sidebar" data-color="purple" data-image="assets/img/sidebar-5.jpg">
+    <div class="wrapper">
+        <div class="sidebar" data-color="green" data-image="assets/img/sidebar-5.jpg">
 
-    <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
+            <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
 
 
-    	<div class="sidebar-wrapper">
-            <div class="logo">
-                <a href="http://www.creative-tim.com" class="simple-text">
-                   APPLICATION
-                </a>
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <a href="http://www.creative-tim.com" class="simple-text">
+                        APPLICATION
+                    </a>
+                </div>
+
+                <ul class="nav">
+                    <li>
+                        <a href="home.php">
+                            <i class="pe-7s-graph"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="monitoring.php">
+                            <i class="pe-7s-graph"></i>
+                            <p>Monitoring</p>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="perhitungan.php">
+                            <i class="pe-7s-user"></i>
+                            <p>Rule Based Combination</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="perhitungan_segitiga.php">
+                            <i class="pe-7s-user"></i>
+                            <p>Rule Based Triangle</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="hasilnya.php">
+                            <i class="pe-7s-user"></i>
+                            <p>Data Results</p>
+                        </a>
+                    </li>
+                    <li class="">
+                        <a href="laporan.php">
+                            <i class="pe-7s-note2"></i>
+                            <p>Report</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="grafik_penyiraman.php">
+                            <i class="pe-7s-graph"></i>
+                            <p>Chart</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="data_user.php">
+                            <i class="pe-7s-science"></i>
+                            <p>User Data</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="logout.php">
+                            <i class="pe-7s-map-marker"></i>
+                            <p>Logout</p>
+                        </a>
+                    </li>
+
+                </ul>
             </div>
-
-            <ul class="nav">
-                <li>
-                    <a href="home.php">
-                        <i class="pe-7s-graph"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-				 <li>
-                    <a href="monitoring.php">
-                        <i class="pe-7s-graph"></i>
-                        <p>Monitoring</p>
-                    </a>
-                </li>
-				
-                <li>
-                    <a href="perhitungan.php">
-                        <i class="pe-7s-user"></i>
-                        <p>Rule Based Combination</p>
-                    </a>
-                </li>
-				<li>
-                    <a href="perhitungan_segitiga.php">
-                        <i class="pe-7s-user"></i>
-                        <p>Rule Based Triangle</p>
-                    </a>
-                </li>
-				 <li>
-                    <a href="hasilnya.php">
-                        <i class="pe-7s-user"></i>
-                        <p>Data Results</p>
-                    </a>
-                </li>
-                <li class="">
-                    <a href="laporan.php">
-                        <i class="pe-7s-note2"></i>
-                        <p>Report</p>
-                    </a>
-                </li>
-				<li>
-                    <a href="grafik_penyiraman.php">
-                        <i class="pe-7s-graph"></i>
-                        <p>Chart</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="data_user.php">
-                        <i class="pe-7s-science"></i>
-                        <p>User Data</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="logout.php">
-                        <i class="pe-7s-map-marker"></i>
-                        <p>Logout</p>
-                    </a>
-                </li>
-             
-            </ul>
-    	</div>
-    </div>
+        </div>
